c13590e471e2cb5e033b6b9cd2da2b84f0e2ce23
matinapap <matinapap180@gmail.com>
2025-07-05 04:19:44 +0300

    Add project files.

---
+++ OnlineShopUniPi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineShopUniPi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
