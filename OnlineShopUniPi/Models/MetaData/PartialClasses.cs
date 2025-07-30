3eee6a38e8df25d6c443c28a871a5dbf581780fb
matinapap <matinapap180@gmail.com>
2025-07-30 19:48:41 +0300

    Created Metadata folder and metadata for user model.

---
+++ OnlineShopUniPi/Models/MetaData/PartialClasses.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopUniPi.Models.MetaData;
+
+namespace OnlineShopUniPi.Models
+{
+    [ModelMetadataType(typeof(UserMetaData))]
+    public partial class User
+    {
+        // Empty class, used only for applying metadata
+    }
+}
---
+++ OnlineShopUniPi/Models/MetaData/UserMetaData.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlineShopUniPi.Models.MetaData
+{
+
+    public partial class UserMetaData
+    {
+        [Display(Name = "User ID")]
+        public int UserId { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; } = null!;
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; } = null!;
+
+        [Display(Name = "Email")]
+        public string Email { get; set; } = null!;
+
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string PasswordHash { get; set; } = null!;
+
+        [Display(Name = "Phone Number")]
+        public string? PhoneNumber { get; set; }
+
+        [Display(Name = "Profile Photo")]
+        public string? ProfilePictureUrl { get; set; }
+
+        [Display(Name = "Address")]
+        public string? Address { get; set; }
+
+        [Display(Name = "City")]
+        public string? City { get; set; }
+
+        [Display(Name = "Country")]
+        public string? Country { get; set; }
+
+        [Display(Name = "Registration Date")]
+        public DateTime? RegistrationDate { get; set; }
+
+        [Display(Name = "Username")]
+        public string Username { get; set; } = null!;
+
+        [Display(Name = "Role")]
+        public string Role { get; set; } = null!;
+
+    }
+
+}
